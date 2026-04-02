5f7ffe9cd7a5016f726723c14a066b7d7587c202
Abraham Neho <abrahamneho@proton.me>
2026-04-02 23:26:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 function saveFile()
 {
 let name = document.getElementById("filename").value;
-
 let content = document.getElementById("filecontent").value;
 
-if(name == "" || content == "")
+if(name === "" || content === "")
 {
 alert("Enter file name and content");
 return;
 }
 
-let display = document.getElementById("output");
+/* Get old files */
+let files = JSON.parse(localStorage.getItem("docs")) || [];
 
-let fileBox = document.createElement("div");
+/* Add new file */
+files.push({
+name:name,
+content:content
+});
 
-fileBox.innerHTML =
-"<h4>"+name+"</h4><p>"+content+"</p><hr>";
+/* Save */
+localStorage.setItem("docs", JSON.stringify(files));
 
-display.appendChild(fileBox);
+displayFiles();
 
 document.getElementById("filename").value="";
 document.getElementById("filecontent").value="";
 }
+
+function displayFiles()
+{
+let files = JSON.parse(localStorage.getItem("docs")) || [];
+
+let output = document.getElementById("output");
+
+output.innerHTML="";
+
+files.forEach(file => {
+
+output.innerHTML +=
+"<h4>"+file.name+"</h4><p>"+file.content+"</p><hr>";
+
+});
+
+}
+
+/* Load saved files when page opens */
+displayFiles();
