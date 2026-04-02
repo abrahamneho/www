15d80399f5cc6b2d8cca0065d38c10b8190393b4
Abraham Neho <abrahamneho@proton.me>
2026-04-02 23:16:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function saveFile()
+{
+let name = document.getElementById("filename").value;
 
+let content = document.getElementById("filecontent").value;
+
+if(name == "" || content == "")
+{
+alert("Enter file name and content");
+return;
+}
+
+let display = document.getElementById("output");
+
+let fileBox = document.createElement("div");
+
+fileBox.innerHTML =
+"<h4>"+name+"</h4><p>"+content+"</p><hr>";
+
+display.appendChild(fileBox);
+
+document.getElementById("filename").value="";
+document.getElementById("filecontent").value="";
+}
